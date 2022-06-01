0bc341873197b32719680a75637f037298a85d04
woosuk-choi0523 <61082292+woosuk-choi0523@users.noreply.github.com>
2022-06-01 16:40:22 +0900

    perfectly

--- index.py
+++ index.py
@@ -1,7 +1,26 @@
 import pandas as pd 
 from pyodide.http import open_url
-url_content = open_url("192.168.0.5/genius/data.csv")
-df = pd.read_csv(url_content)
-#print(df)    
+rl_content = open_url("https://raw.githubusercontent.com/woosuk-choi0523/genius/master/data.csv")
+df=pd.read_csv(rl_content,names=["이름","내용"])
+name = df['이름']
 csv = Element('csv')
-csv.write(df.head())
+csv.write(df)
+k = Element('result')
+
+def linear_search(element, some_list):
+    for i in range(len(some_list)):
+        if element == some_list[i]:
+            return i
+    return -1
+def Search(*args):
+  userinput = Element("userinput")
+  q = userinput.value
+  a = linear_search(q,name)
+  if(a >= 0):
+    h = df.loc[a]
+    k.write(h)
+  else:
+    j= "없음"
+    k.write(j)
+
+  userinput.clear()
